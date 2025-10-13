3e4dc8ce2b126023efc12fba1ddc04fbba8378e5
Harsh Keshri <harshkeshri1234567@gmail.com>
2025-10-13 12:59:39 +0530

    Updated Config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL = process.env.BACKEND_URL || 'http://localhost:6511'
-export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:6011'
+export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:6511'
+export const FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL || 'http://localhost:6011'
